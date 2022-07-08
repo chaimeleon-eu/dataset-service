48dd265a3a68c29431d17d8f7f2952f2852c7e3a
palollo <palollo@i3m.upv.es>
2022-07-08 13:19:55 +0200

    Now metadata is uploaded to Zenodo in PDF format.

--- dataset_service/pid.py
+++ dataset_service/pid.py
@@ -7,6 +7,8 @@ import http.client
 import json
 import base64
 import hashlib
+import io
+from xhtml2pdf import pisa
 
 class PidException(Exception):
     pass
@@ -21,26 +23,10 @@ def createDeposition(connection, url_path, accessToken, dataset, dataset_link_fo
             'upload_type': 'other',
             'title': dataset["name"],
             'description': '''
-                <p>Selection of studies for dataset: 
-                <a href="%s">%s</a><br />
-                Use the link on the dataset ID to access the full contents in a computation platform.</p>
-                <p>---</p>
-                %s
-                <p>---</p>
-                <p>
-                <strong>Studies count: </strong>%d<br />
-                <strong>Subjects count: </strong>%d<br />
-                <strong>Age range: </strong>Between %d %s and %d %s<br />
-                <strong>Sex: </strong>%s<br />
-                <strong>Body part(s): </strong>%s<br />
-                <strong>Modality: </strong>%s<br />
+                <p>Selection of studies for dataset. ID: <a href="%s">%s</a><br />
+                Use the link on the dataset ID to access the full contents in a computation platform.
                 </p>
-                ''' % (dataset_link, dataset["id"], dataset["description"], 
-                       dataset["studiesCount"], dataset["subjectsCount"],
-                       dataset["ageLow"], dataset["ageUnit"][0], dataset["ageHigh"], dataset["ageUnit"][1],
-                       ' ,'.join(dataset["sex"])[:-2], 
-                       ' ,'.join(dataset["bodyPart"])[:-2], 
-                       ' ,'.join(dataset["modality"])[:-2]),
+                ''' % (dataset_link, dataset["id"]),
             'creators': [{'name': dataset["authorName"]}],
             'access_right': 'open',
             'license': 'cc-by',
@@ -69,15 +55,15 @@ def createDeposition(connection, url_path, accessToken, dataset, dataset_link_fo
     publish_url = response["links"]["publish"]
     return bucket_url, publish_url
 
-def uploadFile(connection, bucket_path, accessToken, indexFileJsonContentStr):
+def uploadFile(connection, bucket_path, accessToken, fileName, fileContent):
     headers = {}
     headers['Authorization'] = 'Bearer ' + accessToken
     # headers['Content-Type'] = 'application/json;charset=UTF-8'
     # headers['Content-Type'] = 'text/plain'
     headers['Content-Type'] = 'application/octet-stream'
-    payload = encode(indexFileJsonContentStr)
+    payload = encode(fileContent) if isinstance(fileContent, str) else fileContent
     #logging.root.debug(payload)
-    connection.request("PUT", bucket_path + "/index.json", payload, headers)
+    connection.request("PUT", bucket_path + "/" + fileName, payload, headers)
     res = connection.getresponse()
     httpStatusCode = res.status
     msg = res.read()  # whole response must be readed in order to do more requests using the same connection
@@ -102,6 +88,40 @@ def publishDeposition(connection, publish_path, accessToken):
     response = json.loads(msg)
     return response["doi_url"]
 
+def generateDescriptionHtml(dataset, dataset_link_format):
+    dataset_link = dataset_link_format % dataset["id"]
+    htmlStr = '''
+                %s
+                <p>---</p>
+                <p>
+                <strong>ID: </strong>%s</a><br />
+                <strong>URL: </strong><a href="%s">%s</a><br />
+                <strong>Creation date: </strong>%s<br />
+                <strong>License: </strong>%s<br />
+                <strong>Contact info.: </strong>%s<br />
+                ---<br />
+                <strong>Studies count: </strong>%d<br />
+                <strong>Subjects count: </strong>%d<br />
+                <strong>Age range: </strong>Between %d %s and %d %s<br />
+                <strong>Sex: </strong>%s<br />
+                <strong>Body part(s): </strong>%s<br />
+                <strong>Modality: </strong>%s<br />
+                </p>
+                ''' % (dataset["description"], dataset["id"], dataset_link, dataset_link, 
+                       dataset["creationDate"], dataset["licenseUrl"], dataset["contactInfo"],
+                       dataset["studiesCount"], dataset["subjectsCount"],
+                       dataset["ageLow"], dataset["ageUnit"][0], dataset["ageHigh"], dataset["ageUnit"][1],
+                       ', '.join(dataset["sex"]), 
+                       ', '.join(dataset["bodyPart"]), 
+                       ', '.join(dataset["modality"]))
+    return htmlStr
+
+def generateDescriptionPdf(dataset, dataset_link_format):
+    htmlString = generateDescriptionHtml(dataset, dataset_link_format)
+    pdf = io.BytesIO()
+    pisa_status = pisa.CreatePDF(htmlString, dest=pdf)
+    return pdf.getvalue()   # bytes
+
 def generateIndexJson(studies): 
     # Clear paths and urls, not relevant to Zenodo
     for study in studies:
@@ -118,12 +138,18 @@ def getZenodoDOI(url, accessToken, dataset, studies, dataset_link_format, commun
     bucket_url, publish_url = createDeposition(connection, zenodo.path, accessToken, dataset, dataset_link_format, community, grant)
     # bucket_url example: "https://zenodo.org/api/files/568377dd-daf8-4235-85e1-a56011ad454b"
     logging.root.debug('Zenodo creation of deposition success.')
+    bucket = urllib.parse.urlparse(bucket_url)
+
+    logging.root.debug('Uploading description file...')
+    descriptionFileBytes = generateDescriptionPdf(dataset, dataset_link_format)
+    # previous connection can be used: host and port in bucket_url should be the same 
+    uploadFile(connection, bucket.path, accessToken, "description.pdf", descriptionFileBytes)
+    logging.root.debug('Zenodo uploading success.')
 
     logging.root.debug('Uploading index.json...')
     indexFileJsonContentStr = generateIndexJson(studies)
-    bucket = urllib.parse.urlparse(bucket_url)
     # previous connection can be used: host and port in bucket_url should be the same 
-    uploadFile(connection, bucket.path, accessToken, indexFileJsonContentStr)
+    uploadFile(connection, bucket.path, accessToken, "index.json", indexFileJsonContentStr)
     logging.root.debug('Zenodo uploading success.')
 
     logging.root.debug('Publishing deposition in Zenodo...')
