fb4e8588dfb783a71827197513274dcb532df979
palollo <palollo@i3m.upv.es>
2024-02-13 20:18:00 +0100

    Update KeycloakAdminAPIClient:
    add methods to get and set user attribute.

--- dataset_service/keycloak.py
+++ dataset_service/keycloak.py
@@ -10,7 +10,9 @@ class KeycloakAdminAPIException(Exception):
     def __init__(self, message: str, error_code: int = 0):
         super().__init__(message)
         self.error_code = error_code
-    
+
+#API SPEC: https://www.keycloak.org/docs-api/22.0.5/rest-api/index.html#_users
+
 class KeycloakAdminAPIClient:
     def __init__(self, authClient: auth.AuthClient, apiURL: str, clientIdForGetUserTokens: str):
         self.apiURL = urllib.parse.urlparse(apiURL)
@@ -49,14 +51,18 @@ class KeycloakAdminAPIClient:
             raise e
         raise Exception("Unable to connect to KeycloakAdminAPI.")
 
-    def _GET_JSON(self, path):
+    def _get_connection(self):
         if self.apiURL.hostname is None: raise Exception('Wrong apiUrl.')
-        connection = http.client.HTTPSConnection(self.apiURL.hostname, self.apiURL.port)
+        return http.client.HTTPSConnection(self.apiURL.hostname, self.apiURL.port)
+    def _get_headers(self):
         headers = {}
         headers['Authorization'] = 'bearer ' + self.authClient.get_token()
-        payload = ""
+        return headers
+
+    def _GET_JSON(self, path):
+        connection = self._get_connection()
         try:
-            connection.request("GET", self.apiURL.path + path, payload, headers)
+            connection.request("GET", self.apiURL.path + path, body="", headers=self._get_headers())
             res = connection.getresponse()
             httpStatusCode = res.status
             msg = res.read()  # whole response must be readed in order to do more requests using the same connection
@@ -67,6 +73,21 @@ class KeycloakAdminAPIClient:
             raise KeycloakAdminAPIException('Internal server error: KeycloakAdminAPI call failed.', httpStatusCode)
         logging.root.debug('KeycloakAdminAPI call success.')
         return json.loads(msg)
+
+    def _PUT_JSON(self, path, content):
+        connection = self._get_connection()
+        headers = self._get_headers()
+        headers['Content-Type'] = 'application/json'
+        try:
+            connection.request("PUT", self.apiURL.path + path, content, headers)
+            res = connection.getresponse()
+            httpStatusCode = res.status
+        finally:
+            connection.close()
+        if httpStatusCode != 204:
+            logging.root.error('KeycloakAdminAPI error. Code: %d %s' % (httpStatusCode, res.reason))
+            raise KeycloakAdminAPIException('Internal server error: KeycloakAdminAPI call failed.', httpStatusCode)
+        logging.root.debug('KeycloakAdminAPI call success.')
 
     def getClientUID(self, clientId: str) -> str | None:
         logging.root.debug('Getting client UID for clientId "%s" from KeycloakAdminAPI...' % clientId)
@@ -93,9 +114,9 @@ class KeycloakAdminAPIClient:
             logging.root.error('KeycloakAdminAPI response unexpected: %s' % (response))
             raise KeycloakAdminAPIException('Internal server error: unexpected KeycloakAdminAPI response.')
 
-    def getUserGroups(self, userUID):
+    def getUserGroups(self, userID):
         logging.root.debug('Getting user groups from KeycloakAdminAPI...')
-        response = self._GET_JSON("users/"+userUID+"/groups")
+        response = self._GET_JSON("users/"+userID+"/groups")
         groups = []
         try:
             for group in response:
@@ -118,9 +139,31 @@ class KeycloakAdminAPIClient:
             logging.root.error('KeycloakAdminAPI response unexpected: %s' % (response))
             raise KeycloakAdminAPIException('Internal server error: KeycloakAdminAPI response unexpected.')
 
-    def getUserToken(self, userUID) -> dict:
-        # Possible alternative method: impersonation
+    def getUserToken(self, userID) -> dict:
         logging.root.debug('Getting user token from KeycloakAdminAPI...')
         return self._GET_JSON(
-            "clients/"+self.clientUIDForGetUserTokens+"/evaluate-scopes/generate-example-access-token?scope=openid&userId="+userUID)
+            "clients/"+self.clientUIDForGetUserTokens+"/evaluate-scopes/generate-example-access-token?scope=openid&userId="+userID)
+        # Possible alternative method: impersonation
+        # Other alternative method without using that keycloakAdminAPI: use token exchange
+        # https://www.keycloak.org/docs/latest/securing_apps/index.html#direct-naked-impersonation
+        # Indeed you will need to use one of those alternatives if you need a signed and coded token.
 
+    def getUserAttribute(self, userId, attributeName):
+        logging.root.debug('Getting user from KeycloakAdminAPI...')
+        user = self._GET_JSON("users/"+userId)
+        if attributeName in user["attributes"]:
+            return user["attributes"][attributeName][0] # If the attribute is repeated there will be more than on items, 
+                                                        # but let's return only the first value.
+        else: return None
+
+    def setUserAttribute(self, userId, attributeName, attributeValue):
+        logging.root.debug('Getting user from KeycloakAdminAPI...')
+        user = self._GET_JSON("users/"+userId)
+        if not attributeName in user["attributes"]:
+            attributeValues = [attributeValue]
+        else:
+            attributeValues = user["attributes"][attributeName]
+            attributeValues[0] = attributeValue
+        user["attributes"][attributeName] = attributeValues
+        logging.root.debug('Setting user attribute with KeycloakAdminAPI...')
+        self._PUT_JSON("users/"+userId, json.dumps(user))
